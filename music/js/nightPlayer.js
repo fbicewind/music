df1200a90edf0acc8ab9d344e4240a28ab7ebff3
fbicewind <939228292@qq.com>
2017-12-27 19:31:31 +0800

    version 2017.12.27

---
+++ music/js/nightPlayer.js
@@ -0,0 +1,48 @@
+var nightPlayer = {
+	init : function(){
+
+	},
+	operation : {
+		//上一首
+		prev : function(){
+
+		},
+		//下一首
+		next : function(){
+
+		},
+		//播放
+		play : function(){
+
+		},
+		//暂停
+		pause : function(){
+
+		},
+		//显示隐藏列表
+		toggleList : function(){
+
+		},
+		//静音
+		toggleMute : function(){
+
+		},
+		//随机播放
+		switchMode : function(){
+
+		},
+		//指定页音乐
+		switchMusic : function(index){
+
+		},
+		//调整音量
+		changeVolume : function(){
+
+		}
+	},
+	option : {
+		autoplay : false,
+		volume : 1,
+		random : false
+	}
+}
